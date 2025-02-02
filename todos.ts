2228e27ea41d9e666eb75ce1e82a87fd7c74cc68
AbanobRafik <bobarafik0123@gmail.com>
2025-02-02 18:59:58 +0200

    add todo.ts to know what should i make

---
+++ todos.ts
@@ -0,0 +1,9 @@
+/**
+ * todo make register logic
+ * todo make login logic
+ * todo make logout logic
+ * todo make calling api logic for login and register
+ * todo make calling api logic for logout
+ * *=====================================
+ * todo make calling api for movies
+ * */
